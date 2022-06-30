25a8108035ffd23a0afea1b8ebc54078e8b64377
Parth Vyas <parth@nclouds.com>
2022-06-30 16:54:09 +0530

    fix typo
    Signed-off-by: Parth Vyas <parth@nclouds.com>

--- nops-aws-account-register/cicd/package.sh
+++ nops-aws-account-register/cicd/package.sh
@@ -3,17 +3,17 @@
 #### setup version
 VERSION=$(cat Release_Version)
 
-mkdir packges
+mkdir packages
 
 ## gather cloudformation templates
-cp cloudformation-single-acc-register/nops_register_aws_acc.yaml packges/
-cp cloudformation-single-acc-register/nops_register_aws_acc.yaml packges/nops_register_aws_acc-${VERSION}.yaml
+cp cloudformation-single-acc-register/nops_register_aws_acc.yaml packages/
+cp cloudformation-single-acc-register/nops_register_aws_acc.yaml packages/nops_register_aws_acc-${VERSION}.yaml
 
-cp cloudformation-org-member-accounts-register/member_consolidated_aws_acc_nops_register.yaml packges/
-cp cloudformation-org-member-accounts-register/member_consolidated_aws_acc_nops_register.yaml packges/member_consolidated_aws_acc_nops_register-${VERSION}.yaml
+cp cloudformation-org-member-accounts-register/member_consolidated_aws_acc_nops_register.yaml packages/
+cp cloudformation-org-member-accounts-register/member_consolidated_aws_acc_nops_register.yaml packages/member_consolidated_aws_acc_nops_register-${VERSION}.yaml
 
-cp cloudformation-org-member-accounts-register/cloudformation_execution_role.yaml packges/
-cp cloudformation-org-member-accounts-register/cloudformation_execution_role.yaml packges/cloudformation_execution_role-${VERSION}.yaml
+cp cloudformation-org-member-accounts-register/cloudformation_execution_role.yaml packages/
+cp cloudformation-org-member-accounts-register/cloudformation_execution_role.yaml packages/cloudformation_execution_role-${VERSION}.yaml
 
 
 ## gather lambda code zip files
@@ -29,8 +29,9 @@ do
   pip install -r requirements.txt -t .
   zip -r ${LAMBDA_PACKAGE}.zip .
   cp ${LAMBDA_PACKAGE}.zip ${LAMBDA_PACKAGE}-${VERSION}.zip
-  mv ${LAMBDA_PACKAGE}.zip ../../packges
-  mv ${LAMBDA_PACKAGE}-${VERSION}.zip ../../packges
+  mv ${LAMBDA_PACKAGE}.zip ../../packages
+  mv ${LAMBDA_PACKAGE}-${VERSION}.zip ../../packages
   cd -
 done
 
+ls -l packages
