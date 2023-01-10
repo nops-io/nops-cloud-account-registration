d6fedccd781959052ce85e18c2d506163cfb4b76
parth vyas <parth@nclouds.com>
2023-01-10 19:00:27 +0530

    change child registration from OU to root OU only
    Signed-off-by: parth vyas <parth@nclouds.com>

--- nops-aws-account-register/terraform-multiple-child-accs-register-via-stacksets/examples/complete/main.tf
+++ nops-aws-account-register/terraform-multiple-child-accs-register-via-stacksets/examples/complete/main.tf
@@ -21,8 +21,6 @@ module "nops_aws_acc_register" {
   BucketName              = var.BucketName
   nOpsApiKey              = var.nOpsApiKey
   nOpsPrivateKey          = var.nOpsPrivateKey
-  AccNamePreFixToRegister = var.AccNamePreFixToRegister
-  list_of_OU_ids          = var.list_of_OU_ids
   org_root_id             = var.org_root_id
   tags                    = var.tags
 }
